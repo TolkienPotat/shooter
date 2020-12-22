e80677b81edb0ae3b626728fcce49f2cc9b47237
jack <jack@jack-desktop>
2020-12-22 12:03:32 -0800

    Added bodies

--- tryagain/src/things/EnemyHandler.java
+++ tryagain/src/things/EnemyHandler.java
@@ -2,13 +2,19 @@ package things;
 
 import java.util.ArrayList;
 
+import dev.draw.Renderer;
+
 public class EnemyHandler {
 
 	public ArrayList<Enemy> enemies;
 	
+	BodyHandler bodyHandler;
+	
 	public EnemyHandler() {
 		
 		enemies = new ArrayList<Enemy>();
+		
+		bodyHandler = new BodyHandler();
 		
 	}
 	
@@ -19,30 +25,43 @@ public class EnemyHandler {
 		enemies.add(new Enemy());
 		enemies.get(enemies.size() - 1).xInGame = x;
 		enemies.get(enemies.size() - 1).yInGame = y;
-		System.out.println("Summoning. at " + x + " " + y);
+//		System.out.println("Summoning. at " + x/40 + " " + y/40);
 		
 	}
 	
-	public void delete(int enemy) {
+	public void delete(int enemy, boolean dead) {
+		
+		
+		if (dead) {
+			bodyHandler.add(enemies.get(enemy).xInGame, enemies.get(enemy).yInGame);
+		}
 		enemies.remove(enemy);
 	}
 	
 	public void tick(Player p, Map m) {
-		System.out.println(enemies.size());
+		
 		for (int i = 0; i < enemies.size(); i++) {
-			if (enemies.get(i).health <= 0 || enemies.get(i).shouldKillNextTick == true) {
-				delete(i);
+			if (enemies.get(i).health <= 0) {
+				delete(i, true);
+				continue;
+			} else if (enemies.get(i).shouldKillNextTick == true) {
+				delete(i, true);
 				continue;
 			}
 			enemies.get(i).tick(p, m);
 		}
 		
+		bodyHandler.tick();
+		
 	}
 	
-	public void render() {
+	public void render(Renderer r, Player p) {
 		for (int i = 0; i < enemies.size(); i++) {
 			enemies.get(i).draw();
 		}
+		
+		bodyHandler.render(r, p);
+		
 	}
 	
 }
